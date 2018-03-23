09c9c41c3f4990eabe39d81c491755d012d207d9
Nick Sharp <nick.sharp@urbanmassage.com>
2018-03-23 11:50:21 +0000

    changing db release script

--- create_octopus_db_release.sh
+++ create_octopus_db_release.sh
@@ -7,7 +7,7 @@ OCTOPUS_PROJECTS_URL="${OCTOPUS_BASE}/api/projects/all"
 PROJECT_NAME="${CIRCLE_PROJECT_REPONAME}_db.${BUILD_NO}"
 ZIP_FILE="${PROJECT_NAME}.zip"
 
-zip -r "${ZIP_FILE}" "${RELEASE_DIR}/deploy_db.yml"
+zip -r "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}/deploy_db.yml"
 mv "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}/${ZIP_FILE}"
 
 post_status_code=$(curl -s --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/packages/raw -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -F "data=@${RELEASE_DIR}/${ZIP_FILE}")
@@ -27,7 +27,7 @@ namelist=(${names//,/ })
 idslist=(${ids//,/ })
 
 for i in ${!namelist[@]}; do
-    if [[ ${namelist[$i]} == "${OCTOPUS_PROJECT_NAME}-db" ]];
+    if [[ ${namelist[$i]} == "${CIRCLE_PROJECT_REPONAME}-db" ]];
     then
         project_id=${idslist[$i]}
         status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"Channels-1\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
