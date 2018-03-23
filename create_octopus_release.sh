aa92875c8e78a830f0e08f70984226283132433e
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-23 09:16:43 +0000

    Update create_octopus_release.sh

--- create_octopus_release.sh
+++ create_octopus_release.sh
@@ -4,12 +4,12 @@ BASE_DIR=$(dirname $0)
 RELEASE_DIR=$BASE_DIR/deploy
 
 zip -r $CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip $RELEASE_DIR/deploy.yml
-mv $CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip $RELEASE_DIR/$CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip
+mv $BASE_DIR/$CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip $RELEASE_DIR/$CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip
 
 curl -X POST http://$OCTOPUS_BASE/api/packages/raw -H "X-Octopus-ApiKey:$OCTO_API_KEY" -F "data=@${RELEASE_DIR}/$CIRCLE_PROJECT_REPONAME.$BUILD_NO.zip"
 
-names=$(curl -s 'http://$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Name')
-ids=$(curl -s 'http://$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Id')
+names=$(curl -s '$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Name')
+ids=$(curl -s '$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Id')
 
 names="${names//\"}"
 ids="${ids//\"}"
@@ -22,7 +22,7 @@ for i in ${!namelist[@]}; do
     then
         project_id=${idslist[$i]}
 
-        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST http://$OCTOPUS_BASE/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"Channels-1\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
+        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST $OCTOPUS_BASE/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"Channels-1\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
 
         if [ $status_code -ge 300 ];
         then
