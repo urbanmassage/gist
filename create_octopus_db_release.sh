6e389ceaa785f0ae2fc75c191d3319203adcb932
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-23 11:43:14 +0000

    Update create_octopus_db_release.sh

--- create_octopus_db_release.sh
+++ create_octopus_db_release.sh
@@ -27,7 +27,7 @@ namelist=(${names//,/ })
 idslist=(${ids//,/ })
 
 for i in ${!namelist[@]}; do
-    if [[ ${namelist[$i]} == "${CIRCLE_PROJECT_REPONAME}" ]];
+    if [[ ${namelist[$i]} == "${OCTOPUS_PROJECT_NAME}-db" ]];
     then
         project_id=${idslist[$i]}
         status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"Channels-1\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
