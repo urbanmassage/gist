490306cb7956553b11ba220ef1301219fce4c74e
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-23 09:20:12 +0000

    Update create_octopus_release.sh

--- create_octopus_release.sh
+++ create_octopus_release.sh
@@ -6,7 +6,7 @@ RELEASE_DIR=$BASE_DIR/deploy
 zip -r $CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip $RELEASE_DIR/deploy.yml
 mv $BASE_DIR/$CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip $RELEASE_DIR/$CIRCLE_PROJECT_REPONAME.${BUILD_NO}.zip
 
-curl -X POST http://$OCTOPUS_BASE/api/packages/raw -H "X-Octopus-ApiKey:$OCTO_API_KEY" -F "data=@${RELEASE_DIR}/$CIRCLE_PROJECT_REPONAME.$BUILD_NO.zip"
+curl -X POST $OCTOPUS_BASE/api/packages/raw -H "X-Octopus-ApiKey:$OCTO_API_KEY" -F "data=@${RELEASE_DIR}/$CIRCLE_PROJECT_REPONAME.$BUILD_NO.zip"
 
 names=$(curl -s '$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Name')
 ids=$(curl -s '$OCTOPUS_BASE/api/projects/all' -H "X-Octopus-ApiKey:$OCTO_API_KEY" | jq '.[] | .Id')
