eea4c75047ab2eddcd375c1622a7c683cdf0a300
Nick Sharp <nick.sharp@urbanmassage.com>
2018-03-23 12:08:41 +0000

    Making script call for channel id

--- create_octopus_db_release.sh
+++ create_octopus_db_release.sh
@@ -30,7 +30,8 @@ for i in ${!namelist[@]}; do
     if [[ ${namelist[$i]} == "${CIRCLE_PROJECT_REPONAME}-db" ]];
     then
         project_id=${idslist[$i]}
-        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"Channels-1\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
+        channel_id=$(curl -s ${OCTOPUS_BASE}/api/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
+        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
         if [ ${status_code} -ge 300 ];
         then
             exit ${status_code}
