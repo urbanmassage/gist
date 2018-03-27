f2f04cc4f092347fb2b4b2f46784e4cb894790d5
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-27 09:41:14 +0100

    Update create_octopus_release.sh

--- create_octopus_release.sh
+++ create_octopus_release.sh
@@ -33,7 +33,7 @@ for i in ${!namelist[@]}; do
         project_id=${idslist[$i]}
         channel_id=$(curl -s ${OCTOPUS_FULL_BASE}/api/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
         channel_id="${channel_id//\"}"
-        branch_name="${CIRCLE_BRANCH//_/\_}"
+        branch_name="${CIRCLE_BRANCH//_/\\\\\\_}"
         status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${branch_name}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
         if [ ${status_code} -ge 300 ];
         then
