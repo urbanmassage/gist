8d73fe960571c1e213a5cf2c3ca99b0dbd1b9b07
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-27 09:29:10 +0100

    Update create_octopus_release.sh

--- create_octopus_release.sh
+++ create_octopus_release.sh
@@ -2,7 +2,8 @@ set -f
 
 BASE_DIR=$(dirname $0)
 RELEASE_DIR="${BASE_DIR}/deploy"
-OCTOPUS_PROJECTS_URL="https://${OCTOPUS_BASE}/api/projects/all"
+OCTOPUS_FULL_BASE="https://${OCTOPUS_BASE}/api"
+OCTOPUS_PROJECTS_URL="${OCTOPUS_FULL_BASE}/projects/all"
 
 PROJECT_NAME="${CIRCLE_PROJECT_REPONAME}.${BUILD_NO}"
 ZIP_FILE="${PROJECT_NAME}.zip"
@@ -10,7 +11,7 @@ ZIP_FILE="${PROJECT_NAME}.zip"
 zip -r "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}/deploy.yml"
 mv "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}/${ZIP_FILE}"
 
-post_status_code=$(curl -s --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/packages/raw -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -F "data=@${RELEASE_DIR}/${ZIP_FILE}")
+post_status_code=$(curl -s --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/api/packages/raw -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -F "data=@${RELEASE_DIR}/${ZIP_FILE}")
 
 if [ ${post_status_code} -ge 300 ];
 then
@@ -30,10 +31,10 @@ for i in ${!namelist[@]}; do
     if [[ ${namelist[$i]} == "${CIRCLE_PROJECT_REPONAME}" ]];
     then
         project_id=${idslist[$i]}
-        channel_id=$(curl -s ${OCTOPUS_BASE}/api/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
+        channel_id=$(curl -s ${OCTOPUS_FULL_BASE}/api/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
         channel_id="${channel_id//\"}"
         branch_name="${CIRCLE_BRANCH//_/\_}"
-        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${branch_name}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
+        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/api/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${branch_name}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}")
         if [ ${status_code} -ge 300 ];
         then
             exit ${status_code}
