c4095b871dd3755a5af6e5923eb1cbcca976c326
Nick Sharp <sharpn@users.noreply.github.com>
2018-03-28 10:39:51 +0100

    Update create_octopus_release.sh

--- create_octopus_release.sh
+++ create_octopus_release.sh
@@ -34,7 +34,7 @@ for i in ${!namelist[@]}; do
         channel_id=$(curl -s ${OCTOPUS_FULL_BASE}/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
         channel_id="${channel_id//\"}"
         
-        branch_name = "${CIRCLE_BRANCH//_/-}"
+        branch_name="${CIRCLE_BRANCH//_/-}"
 
         post_json="{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${branch_name}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${BUILD_NO}\"}]}"       
         status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "${post_json}")
