87d27eb30873b5b77234c3b3e9e3b970db77f7c2
Nick Sharp <sharpn@users.noreply.github.com>
2018-04-17 10:21:19 +0100

    Update yml_lint.sh

--- yml_lint.sh
+++ yml_lint.sh
@@ -1,6 +1,7 @@
 exitCode=0
 
 deployment_folder="./deploy"
+deployment_file="deploy.yml"
 
 run(){
     $@
@@ -14,12 +15,14 @@ yamllinter(){
     printf "\nLinting the deploy.yml at ${1}\n"
 
     #If the deploy.yml file is present then lint it. Otherwise report an error as one must exist
-    if [ -e $1/deploy.yml ]; then
-        run yamllint -c test/circle/lintconf.yml $1/Chart.yaml
+    if [ -e $1/${deployment_file} ]; then
+        run yamllint -c https://raw.githubusercontent.com/urbanmassage/gist/master/lint_conf.yml $1/${deployment_file}
     else
-        echo "Error $1/deploy.yml file is missing"
+        echo "Error $1/${deployment_file} file is missing"
         exitCode=1
     fi
 }
 
 yamllinter ${deployment_folder}
+
+exit $exitCode
