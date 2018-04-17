806cb93793cc8716edf95e273794f5de6982ce11
Nick Sharp <sharpn@users.noreply.github.com>
2018-04-17 10:27:04 +0100

    Update yml_lint.sh

--- yml_lint.sh
+++ yml_lint.sh
@@ -1,5 +1,3 @@
-pip install yamllint==1.8.1
-
 exitCode=0
 
 deployment_folder="./deploy"
@@ -18,7 +16,9 @@ yamllinter(){
 
     #If the deploy.yml file is present then lint it. Otherwise report an error as one must exist
     if [ -e $1/${deployment_file} ]; then   
-        run yamllint -c https://raw.githubusercontent.com/urbanmassage/gist/master/lint_conf.yml $1/${deployment_file}
+        wget -O $1/lint_conf.yml https://raw.githubusercontent.com/urbanmassage/gist/master/lint_conf.yml
+    
+        run yamllint -c $1/lint_conf.yml $1/${deployment_file}
     else
         echo "Error $1/${deployment_file} file is missing"
         exitCode=1
