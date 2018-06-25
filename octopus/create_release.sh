f11e175a029ea3e7f0152d7209ef74c197190d7d
Nick Sharp <nick.sharp@urbanmassage.com>
2018-06-25 11:03:29 +0100

    Adding new scripts

---
+++ octopus/create_release.sh
@@ -0,0 +1,39 @@
+set -f
+
+BASE_DIR=$(dirname $0)
+RELEASE_DIR="${BASE_DIR}/deploy"
+OCTOPUS_FULL_BASE="https://${OCTOPUS_BASE}/api"
+OCTOPUS_PROJECTS_URL="${OCTOPUS_FULL_BASE}/projects/all"
+
+CURRENT_BUILD_NO=$1
+CURRENT_PROJECT_NAME=$2
+CURRENT_BRANCH_NAME="${3//_/-}"
+
+PROJECT_NAME="${CURRENT_PROJECT_NAME}.${CURRENT_BUILD_NO}"
+ZIP_FILE="${PROJECT_NAME}.zip"
+
+names=$(curl --silent ${OCTOPUS_PROJECTS_URL} -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.[] | .Name')
+ids=$(curl --silent ${OCTOPUS_PROJECTS_URL} -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.[] | .Id')
+
+names="${names//\"}"
+ids="${ids//\"}"
+
+namelist=(${names//,/ })
+idslist=(${ids//,/ })
+
+for i in ${!namelist[@]}; do
+    if [[ ${namelist[$i]} == "${CURRENT_PROJECT_NAME}" ]];
+    then
+        project_id=${idslist[$i]}
+        channel_id=$(curl -s ${OCTOPUS_FULL_BASE}/projects/${project_id}/channels?take=1 -H "X-Octopus-ApiKey:${OCTO_API_KEY}" | jq '.Items[0].Id')
+        channel_id="${channel_id//\"}"
+
+        post_json="{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${CURRENT_BRANCH_NAME}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [{\"StepName\": \"Unpack Deployment Assets\",\"ActionName\": \"Unpack Deployment Assets\",\"Version\": \"${CURRENT_BUILD_NO}\"}]}"       
+        status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "${post_json}")
+        if [ ${status_code} -ge 300 ];
+        then
+            exit ${status_code}
+        fi
+        break
+    fi
+done
---
+++ octopus/push_package.sh
@@ -0,0 +1,23 @@
+set -f
+
+BASE_DIR=$(dirname $0)
+RELEASE_DIR="${BASE_DIR}/deploy"
+OCTOPUS_FULL_BASE="https://${OCTOPUS_BASE}/api"
+OCTOPUS_PROJECTS_URL="${OCTOPUS_FULL_BASE}/projects/all"
+
+CURRENT_BUILD_NO=$1
+CURRENT_PROJECT_NAME=$2
+CURRENT_BRANCH_NAME="${3//_/-}"
+
+PROJECT_NAME="${CURRENT_PROJECT_NAME}.${CURRENT_BUILD_NO}"
+ZIP_FILE="${PROJECT_NAME}.zip"
+
+zip -r "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}"
+mv "${BASE_DIR}/${ZIP_FILE}" "${RELEASE_DIR}/${ZIP_FILE}"
+
+post_status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/packages/raw -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -F "data=@${RELEASE_DIR}/${ZIP_FILE}")
+
+if [ ${post_status_code} -ge 300 ];
+then
+    exit ${post_status_code};
+fi
