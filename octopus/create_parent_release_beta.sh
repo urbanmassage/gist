6af4ac6d6befb25a65d5df3c6ba3068e6aa4ff26
Nick Sharp <nick.sharp@urbanmassage.com>
2018-06-28 16:16:08 +0100

    outputing from create release

--- octopus/create_parent_release_beta.sh
+++ octopus/create_parent_release_beta.sh
@@ -44,7 +44,7 @@ GenerateDeploySteps(){
 
 CreateRelease(){
     post_json="{\"ProjectId\":\"${project_id}\", \"ReleaseNotes\":\"Branch: ${CURRENT_BRANCH_NAME}\", \"Version\":\"${BUILD_NO}\", \"ChannelId\":\"${channel_id}\",\"SelectedPackages\": [$DEPLOY_STEPS]}" 
-    status_code=$(curl --silent --output /dev/null -w "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "${post_json}")
+    status_code=$(curl --silent --output /dev/stderr -w "%{http_code}" -X POST ${OCTOPUS_FULL_BASE}/releases -H "X-Octopus-ApiKey:${OCTO_API_KEY}" -H "content-type:application/json" -d "${post_json}")
     if [ ${status_code} -ge 300 ];
     then
         printf "${RED}Upload to Octopus was not successful"
