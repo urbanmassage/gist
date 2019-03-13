6d2b1bb4177a1da5526c7ccf073a159b0335a0ac
Nick Sharp <nicksharp@nicks-mbp-2.home>
2019-03-13 21:23:45 +0000

    changing octo

--- octo.sh
+++ octo.sh
@@ -6,8 +6,13 @@ function octo(){
     APPLICATION_NAME=${CIRCLE_PROJECT_REPONAME}
     BASE_PATH="./deploy"
 
+    PUBLISH=false
+    CREATE_RELEASE=false
+
     for i in "$@"; do
         case $1 in
+            --publish) PUBLISH=true ;;
+            --release) CREATE_RELEASE=true ;;
             --buildno) BUILD_NUMBER="$2"; shift ;;
             --appname) APPLICATION_NAME="$2"; shift ;;
             --basepath) BASE_PATH="$2"; shift ;;
@@ -22,9 +27,10 @@ function octo(){
     PUSH_COMMAND="--package=${APPLICATION_NAME}.${BUILD_NUMBER}.zip --replace-existing ${CREDENTIALS}"
     CREATE_RELEASE_COMMAND="--project=${APPLICATION_NAME} --version=${BUILD_NUMBER} --packageversion=${BUILD_NUMBER} ${CREDENTIALS}"
 
-    docker run --rm -v $(pwd):/src octopusdeploy/octo pack ${PACKAGE_COMMAND}
-    docker run --rm -v $(pwd):/src octopusdeploy/octo push ${PUSH_COMMAND}
-    docker run --rm -v $(pwd):/src octopusdeploy/octo create-release ${CREATE_RELEASE_COMMAND}
-
-    ;
+    if $CREATE_RELEASE; then
+        docker run --rm -v $(pwd):/src octopusdeploy/octo pack ${PACKAGE_COMMAND}
+        docker run --rm -v $(pwd):/src octopusdeploy/octo push ${PUSH_COMMAND}
+    else
+        docker run --rm -v $(pwd):/src octopusdeploy/octo create-release ${CREATE_RELEASE_COMMAND}
+    fi
 }
