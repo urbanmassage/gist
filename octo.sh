f0a0639420ae59cbfc88769ca5fd0daa3428a0d2
Nick Sharp <nick.sharp@urbanmassage.com>
2019-05-10 16:13:24 +0100

    adding branch to release notes of release

--- octo.sh
+++ octo.sh
@@ -4,6 +4,7 @@ OCTOPUS_API_KEY="${OCTO_API_KEY}"
 BUILD_NUMBER=${BUILD_NO}
 APPLICATION_NAME=${CIRCLE_PROJECT_REPONAME}
 BASE_PATH="./deploy"
+BRANCH="${CIRCLE_BRANCH}"
 
 PUBLISH=false
 CREATE_RELEASE=false
@@ -15,16 +16,19 @@ for i in "$@"; do
         --buildno) BUILD_NUMBER="$2"; shift ;;
         --appname) APPLICATION_NAME="$2"; shift ;;
         --basepath) BASE_PATH="$2"; shift ;;
+        --branch) BRANCH="$2"; shift;; 
     esac
     shift
 done
+
+BRANCH="${BRANCH//_/\"_\"}" #Escaping _ because they mean italic in markdown
 
 CREDENTIALS="--server=${OCTOPUS_FULL_BASE} --apiKey=${OCTOPUS_API_KEY}"
 
 # PACKAGE
 PACKAGE_COMMAND="--id=${APPLICATION_NAME} --format=zip --version=${BUILD_NUMBER} --overwrite"
 PUSH_COMMAND="--package=${APPLICATION_NAME}.${BUILD_NUMBER}.zip --replace-existing ${CREDENTIALS}"
-CREATE_RELEASE_COMMAND="--project=${APPLICATION_NAME} --version=${BUILD_NUMBER} --packageversion=${BUILD_NUMBER} ${CREDENTIALS}"
+CREATE_RELEASE_COMMAND="--project=${APPLICATION_NAME} --version=${BUILD_NUMBER} --packageversion=${BUILD_NUMBER} --releasenotes=\"Branch:${BRANCH}\" ${CREDENTIALS}"
 
 if $PUBLISH; then
     docker create -v /src --name data alpine:3.4 /bin/true
