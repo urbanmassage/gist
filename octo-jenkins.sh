f38810ba3322ba8342c829f86cba42ae84ed2bdb
Giles Williams <giles@urban.co>
2020-06-26 13:50:49 +0100

    fixes DEPLOY_TO in octo-jenkins.sh

--- octo-jenkins.sh
+++ octo-jenkins.sh
@@ -38,7 +38,7 @@ for deploy_target in "${deploy_targets[@]}"
 # PACKAGE
 PACKAGE_COMMAND="--id=${APPLICATION_NAME} --format=zip --version=${BUILD_NUMBER} --overwrite"
 PUSH_COMMAND="--package=${APPLICATION_NAME}.${BUILD_NUMBER}.zip --replace-existing ${CREDENTIALS}"
-CREATE_RELEASE_COMMAND="--project=${APPLICATION_NAME} --version=${BUILD_NUMBER} --packageversion=${BUILD_NUMBER} --releasenotes=${RELEASE_NOTES} ${DEPLOYTO}${CREDENTIALS}"
+CREATE_RELEASE_COMMAND="--project=${APPLICATION_NAME} --version=${BUILD_NUMBER} --packageversion=${BUILD_NUMBER} --releasenotes=${RELEASE_NOTES} ${DEPLOY_TO}${CREDENTIALS}"
 
 if $PUBLISH; then
     docker create -v /src --name octopus-data alpine:3.4 /bin/true
